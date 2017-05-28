1bbdd59757aaa21c584b1d04f0329cffb76c07fa
collins lenjo <collinslenjo.cl@gmail.com>
2017-05-28 21:32:03 +0300

    app layout

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,1 +1,7 @@
+@extends('layout.app')
+
+@section('content')
+
 <h1>Contact</h1>
+
+@endsection
---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Larasite</title>
+</head>
+<body>
+
+@yield('content')
+
+@section('sidebar')
+<div class="sidebar">
+	<h3>Sidebar</h3>
+	<p>This is the sidebar</p>
+</div>
+
+</body>
+</html>
