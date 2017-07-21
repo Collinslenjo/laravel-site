0a2daca8923e1f4652855873d5344f1ab0586559
collins lenjo <collinslenjo.cl@gmail.com>
2017-07-21 12:43:33 +0300

    message controller

--- app/Http/Controllers/ViewController.php
+++ app/Http/Controllers/ViewController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class ViewController extends Controller
 {
-    //
+	private function __construct(){
+		//Constructor
+	}
+
+    public function message(){
+    	//Message logic coming here
+    }
 }
